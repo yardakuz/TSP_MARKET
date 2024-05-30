94dbbe39644718daeac16b29bb9ebc7c1845d76c
vitalikx100 <vitalikstarigin@mail.ru>
2024-05-31 00:08:26 +0400

    Create Order.java
    Order

---
+++ src/main/java/com/example/tsp_market/models/Order.java
@@ -0,0 +1,53 @@
+package com.example.tsp_market.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "Orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "userID")
+    private String userID;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "isActive")
+    private boolean isActive;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn
+    private User user;
+    private LocalDateTime dateOfCreated;
+
+    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinTable(name = "order-technique",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "technique_id")
+    )
+    private List<Technique> techniques;
+
+
+    @PrePersist
+    private void init(){
+        dateOfCreated = LocalDateTime.now();
+    }
+
+
+
+}
